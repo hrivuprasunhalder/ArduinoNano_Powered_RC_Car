8f9ef2244af86eb3144f48c0f445988515890b43
Hrivu <hrivuprasunhalder@gmail.com>
2026-02-09 07:41:56 +0600

    minor

--- nanorccar/nanorccar.ino
+++ nanorccar/nanorccar.ino
@@ -1,31 +1,28 @@
 
-int mtr_AB_en= 5;
-int mtr_AB_in1= 6;
-int mtr_AB_in2= 7;
+int mtr_A_en= 5;
+int mtr_A_in1= 6;
+int mtr_A_in2= 7;
 
-int mtr_CD_en= 10;
-int mtr_CD_in1= 8;
-int mtr_CD_in2= 9;
+int mtr_B_en= 10;
+int mtr_B_in1= 8;
+int mtr_B_in2= 9;
 
 int ch1= 2;
 int ch3= 3;
 
 void setup() {
 
- 
-  pinMode(mtr_AB_en,OUTPUT);
-  pinMode(mtr_AB_in1,OUTPUT);
-  pinMode(mtr_AB_in2,OUTPUT);
 
-  pinMode(mtr_CD_en,OUTPUT);
-  pinMode(mtr_CD_in1,OUTPUT);
-  pinMode(mtr_CD_in2,OUTPUT);  
+  pinMode(mtr_A_en,OUTPUT);
+  pinMode(mtr_A_in1,OUTPUT);
+  pinMode(mtr_A_in2,OUTPUT);
+
+  pinMode(mtr_B_en,OUTPUT);
+  pinMode(mtr_B_in1,OUTPUT);
+  pinMode(mtr_B_in2,OUTPUT);  
 
   pinMode(ch1,INPUT);
   pinMode(ch3,INPUT);
-
-
-
 
   
 }
@@ -35,35 +32,32 @@ void loop() {
   int steer= pulseIn(ch1,HIGH,25000);
   int throttle= pulseIn(ch3,HIGH,25000);
 
+
   int speed= map(throttle,1000,2000,25,255);
-  analogWrite(mtr_AB_en,speed);
-  analogWrite(mtr_CD_en,speed);
+  analogWrite(mtr_A_en,speed);
+  analogWrite(mtr_B_en,speed);
   
   
+  digitalWrite(mtr_A_in1,HIGH);
+  digitalWrite(mtr_A_in2,LOW);
+  digitalWrite(mtr_B_in1,HIGH);
+  digitalWrite(mtr_B_in2,LOW);
 
-  digitalWrite(mtr_AB_in1,HIGH);
-  digitalWrite(mtr_AB_in2,LOW);
-  digitalWrite(mtr_CD_in1,HIGH);
-  digitalWrite(mtr_CD_in2,LOW);
+  if(steer>1600){
 
-  if(steer>1700){
-
-    digitalWrite(mtr_AB_in1,LOW);
-    digitalWrite(mtr_AB_in2,HIGH);
-    digitalWrite(mtr_CD_in1,HIGH);
-    digitalWrite(mtr_CD_in2,LOW);
+    digitalWrite(mtr_A_in1,LOW);
+    digitalWrite(mtr_A_in2,HIGH);
+    digitalWrite(mtr_B_in1,HIGH);
+    digitalWrite(mtr_B_in2,LOW);
   }
 
-  else if(steer< 1300)
+  else if(steer< 1400)
   {
-    digitalWrite(mtr_AB_in1,HIGH);
-    digitalWrite(mtr_AB_in2,LOW);
-    digitalWrite(mtr_CD_in1,LOW);
-    digitalWrite(mtr_CD_in2,HIGH);
+    digitalWrite(mtr_A_in1,HIGH);
+    digitalWrite(mtr_A_in2,LOW);
+    digitalWrite(mtr_B_in1,LOW);
+    digitalWrite(mtr_B_in2,HIGH);
   }
 
-  
-
-  
 
 }
